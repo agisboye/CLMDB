1b4df6e31e8c0af7d4a84ae4ae5451cfd0200e4e
agisboye <august@heegaard.org>
2019-08-28 23:29:41 +0200

    Add products and targets to Package file.

--- Package.swift
+++ Package.swift
@@ -3,5 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "CLMDB"
+    name: "CLMDB",
+    
+    products: [
+        .library(name: "LMDB", targets: ["LMDB"]),
+    ],
+    
+    targets: [
+        .target(name: "LMDB"),
+    ]
 )
